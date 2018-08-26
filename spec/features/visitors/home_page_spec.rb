bc41dfae16199f81178ecdf658b0d57b75a78ceb
Matthew Santeler <Matthew.Santeler@lendkey.com>
2018-08-26 19:58:33 -0400

    Spec describe basic homepage

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -7,10 +7,31 @@ feature 'Home page' do
   # Scenario: Visit the home page
   #   Given I am a visitor
   #   When I visit the home page
-  #   Then I see "Welcome"
+  #   Then I see "Sign in"
   scenario 'visit the home page' do
     visit root_path
-    expect(page).to have_content 'Welcome'
+    expect(page).to have_content 'Sign in'
+  end
+
+  # Scenario: Visit the home page
+  #   Given I am signed in
+  #   Then I should see my profile card
+  scenario 'user sees profile card' do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    visit root_path
+    expect(page).to have_content "Profile Card"
+    expect(page).to have_content user.email
+  end
+
+  # Scenario: Visit the home page
+  #   Given I am signed in
+  #   Then I should see some tweets
+  scenario 'user sees profile card' do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    visit root_path
+    expect(page).to have_content "Some Tweet"
   end
 
 end
