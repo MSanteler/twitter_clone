0bc9269f79bcee73b2d2fc3adc97ac45497cebf4
Matthew Santeler <Matthew.Santeler@lendkey.com>
2018-08-26 19:42:13 -0400

    spec Custom navbar behavior

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -19,17 +19,21 @@ feature 'Navigation links', :devise do
   #   Given I am a visitor
   #   Given I am signed in
   #   When I visit the home page
-  #   Then I see "home," "sign in," and "sign up"
+  #   Then I see "home," "moments," "notifications," "messages," and "sign up"
+  #   Then I do not see "sign in" or "sign up"
   scenario 'view navigation links' do
     user = FactoryBot.create(:user)
     login_as(user, :scope => :user)
     visit root_path
     expect(page).to have_content 'Home'
-    expect(page).not_to have_content 'Sign in'
-    expect(page).not_to have_content 'Sign up'
+    expect(page).to have_content 'Moments'
+    expect(page).to have_content 'Notifications'
+    expect(page).to have_content 'Messages'
     expect(page).to have_content 'Edit account'
     expect(page).to have_content 'Sign out'
     expect(page).to have_content 'Users'
+    expect(page).not_to have_content 'Sign in'
+    expect(page).not_to have_content 'Sign up'
   end
 
 end
