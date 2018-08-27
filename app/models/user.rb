c549513ff3fe8e68c97498a44ca9996c9e7cc388
Matthew Santeler <Matthew.Santeler@lendkey.com>
2018-08-26 22:45:15 -0400

    Setup followings and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :follows
+  has_many :followings, through: :follows, source: :followed
+  has_many :reverse_followings,   foreign_key: "followed_id",
+                                  class_name: "Follow",
+                                  dependent: :destroy
+  has_many :followers, through: :reverse_followings, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_010617) do
+ActiveRecord::Schema.define(version: 2018_08_27_023731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["user_id"], name: "index_follows_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.bigint "user_id"
@@ -36,5 +45,6 @@ ActiveRecord::Schema.define(version: 2018_08_27_010617) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "follows", "users"
   add_foreign_key "tweets", "users"
 end
